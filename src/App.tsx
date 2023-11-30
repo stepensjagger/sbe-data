5e10279c2927dc844c7aabad00b03ac7c0e27581
stepensjagger <stephensjagger@gmail.com>
2023-11-30 08:30:00 -0600

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,52 @@
-import usestate from "react"
-
-
-
-
-let timesToLoop:number;
-let dataToLoop:any
-let i:number
-let dataStored: any[] = []
-
-function DataProcessed(dataToLoop: any, timesToLoop: number){
-  for(i = 0;i < timesToLoop; i++ ){
-    dataStored.push(dataToLoop)
-  }return dataStored
-}
-
-
+import React, { useState } from 'react';
 
 function App() {
+  const [timesToLoop, setTimesToLoop] = useState<number>(0);
+  const [dataToLoop, setDataToLoop] = useState<any>(null);
+
+  const handleTimesToLoopChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTimesToLoop(parseInt(event.target.value));
+  };
+
+  const handleDataToLoopChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setDataToLoop(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    const dataProcessed = DataProcessed(dataToLoop, timesToLoop);
+    navigator.clipboard.writeText(dataProcessed.toString());
+  };
+
+
+
+  
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <label>
         <input
           type="number"
-          value={timesToLoop}>
-        </input>
+          name="timesToLoop"
+          id="timesToLoop"
+          onChange={handleTimesToLoopChange}
+        />
         <input
-          value={dataToLoop}
-        >  
-        </input>
-        <button onClick={()=>DataProcessed}>
-          Submit Data
-        </button>
+          name="dataToLoop"
+          id="dataToLoop"
+          onChange={handleDataToLoopChange}
+        />
+        <input type="submit" value="Submit" />
       </label>
     </form>
-
   );
+}
+
+function DataProcessed(dataToLoop: any, timesToLoop: number): any[] {
+  let dataStored: any[] = [];
+  for (let i = 0; i < timesToLoop; i++) {
+    dataStored.push(dataToLoop);
+  }
+  return dataStored;
 }
 
 export default App;
